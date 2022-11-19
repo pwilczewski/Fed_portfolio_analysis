089fa952d18be697880705bb2d27f60c9dd94868
pwilczewski <pwilczewski@users.noreply.github.com>
2022-11-19 10:12:36 -0600

    augmenting table

--- openRMBS.py
+++ openRMBS.py
@@ -243,8 +243,11 @@ def plot_durations(mbs_prices, duration):
   plt.show()
   
 def plot_gap(interest_gap, asof_date):
+    
+  cumulative_gap = interest_gap['gap'].cumsum()*1e-9
   print("\n")
-  print("Cumulative net income: ", round(interest_gap['gap'].cumsum().iloc[-1]*1e-9,1), "billion")
+  print("1-year ahead net income:\t", round(cumulative_gap.iloc[11],1), "billion")
+  print("Cumulative net income:\t", round(cumulative_gap.iloc[-1],1), "billion")
   print("\n")
 
   annual_gap = interest_gap.rolling(12).sum()[11::12]*1e-9
